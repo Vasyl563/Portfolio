405e62a5367cfa61012e4dacb575ea760f5d6bce
Vasyl563 <65411442+Vasyl563@users.noreply.github.com>
2022-07-30 11:25:01 +0300

    last commit

--- js/button.js
+++ js/button.js
@@ -1,9 +1,16 @@
 const mobileNavButton = document.querySelector('.mobile-nav-button');
 const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
 const mobileNav = document.querySelector('.mobile-nav');
+const mobileClick = document.querySelector('.mobile-click')
 
 mobileNavButton.addEventListener('click', function () {
   mobileNavIcon.classList.toggle('active');
 	mobileNav.classList.toggle('active');
   document.body.classList.toggle('no-scroll');
+})
+
+mobileClick.addEventListener('click', function () {
+  mobileNavIcon.classList.remove('active');
+	mobileNav.classList.remove('active');
+  document.body.classList.remove('no-scroll')
 })
